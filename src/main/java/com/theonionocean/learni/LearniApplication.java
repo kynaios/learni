9e29f3a3ca136eb1843dddc4e2f9a0c3222a554a
kynaios <blaise.witczak@gmail.com>
2023-10-05 18:37:19 +0200

    Initalize Java project

---
+++ src/main/java/com/theonionocean/learni/LearniApplication.java
@@ -0,0 +1,13 @@
+package com.theonionocean.learni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theonionocean/learni/LearniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theonionocean.learni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
