103afed59777e37af0155c5331d7013b73fdf8c0
kynaios <blaise.witczak@gmail.com>
2023-10-09 19:12:31 +0200

    Create flash card DTO

---
+++ src/main/java/com/theonionocean/learni/dto/FlashCardDto.java
@@ -0,0 +1,52 @@
+package com.theonionocean.learni.dto;
+
+import com.theonionocean.learni.entity.FlashCard;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class FlashCardDto {
+    private UUID id;
+    private String word;
+    private String translation;
+
+    public FlashCard map() {
+        return new FlashCard(id, word, translation);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FlashCardDto that)) return false;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getWord(), that.getWord()) && Objects.equals(getTranslation(), that.getTranslation());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getWord(), getTranslation());
+    }
+}
