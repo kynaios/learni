147cd7ce8a9eddba25f8cc6c578b16132c974e05
kynaios <blaise.witczak@gmail.com>
2023-10-09 11:51:16 +0200

    Check database connection

--- src/main/java/com/theonionocean/learni/LearniApplication.java
+++ src/main/java/com/theonionocean/learni/LearniApplication.java
@@ -1,13 +1,25 @@
 package com.theonionocean.learni;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class LearniApplication {
+public class LearniApplication implements CommandLineRunner {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public LearniApplication(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearniApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+	}
 }
