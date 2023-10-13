067c5f5661544f3b4addbdf570270632f0e9a34e
kynaios <blaise.witczak@gmail.com>
2023-10-13 14:27:53 +0200

    Create flash_cards_bag table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 DROP TABLE IF EXISTS "flash_cards";
+DROP TABLE IF EXISTS "flash_cards_bag";
 
 CREATE TABLE flash_cards (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    word varchar NOT NULL,
+    translation varchar NOT NULL
+);
+
+CREATE TABLE flash_cards_bag (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
     word varchar NOT NULL,
     translation varchar NOT NULL
