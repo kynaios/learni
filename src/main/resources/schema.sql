80b6573051df2ba4cf908ff8524268834d84df2c
kynaios <blaise.witczak@gmail.com>
2023-10-18 13:38:00 +0200

    Add decks table and drop tables cascade

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,21 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-DROP TABLE IF EXISTS "flash_cards";
-DROP TABLE IF EXISTS "flash_cards_bag";
+DROP TABLE IF EXISTS "decks" CASCADE;
+DROP TABLE IF EXISTS "flash_cards" CASCADE;
+DROP TABLE IF EXISTS "flash_cards_bag" CASCADE;
+
+CREATE TABLE decks (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    name varchar NOT NULL
+);
 
 CREATE TABLE flash_cards (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
     word varchar NOT NULL,
-    translation varchar NOT NULL
+    translation varchar NOT NULL,
+    deck_id uuid NOT NULL,
+
+    CONSTRAINT fk_deckId FOREIGN KEY(deck_id) REFERENCES decks(id)
 );
 
 CREATE TABLE flash_cards_bag (
